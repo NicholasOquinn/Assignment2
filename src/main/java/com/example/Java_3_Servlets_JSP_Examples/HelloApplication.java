65f918dd684d687618fc623854ce52f4a53d6224
JoshTaylorNL <jktylr@gmail.com>
2022-02-02 08:31:58 -0330

    Initial Commit

---
+++ src/main/java/com/example/Java_3_Servlets_JSP_Examples/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.Java_3_Servlets_JSP_Examples;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/com/example/Java_3_Servlets_JSP_Examples/HelloResource.java
@@ -0,0 +1,14 @@
+package com.example.Java_3_Servlets_JSP_Examples;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource {
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
